b00bb84586fd6627c68fef122d52850ae17ce4bc
kaplimonadi <kapilmonadi@gmail.com>
2025-08-11 20:57:40 +0530

    added template client

--- src/main/java/com/kapil/main/TemplateSampleClient.java
+++ src/main/java/com/kapil/main/TemplateSampleClient.java
@@ -1,6 +1,15 @@
 package com.kapil.main;
 
+import com.kapil.TeacherType;
+import com.kapil.factory.TeacherFactory;
 import com.kapil.template.Teacher;
 
 public class TemplateSampleClient {
+    public static void main(String[] args) {
+        Teacher permanentTeacher = TeacherFactory.of(TeacherType.PERMANENT);
+        Teacher temporaryTeacher = TeacherFactory.of(TeacherType.TEMPORARY);
+
+        permanentTeacher.teach();
+        temporaryTeacher.teach();
+    }
 }
